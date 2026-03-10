242321ff78999a1e0a1f1b57a27db97c669922e0
Pranshu Kesharwani <pranshu.kesharwani@entrata.com>
2026-03-10 09:51:43 +0530

    changes as per client

--- forms/contact.php
+++ forms/contact.php
@@ -34,8 +34,7 @@
   */
 
   $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( isset($_POST['phone']) ? $_POST['phone'] : '—', 'Phone');
-  $contact->add_message( isset($_POST['email']) ? $_POST['email'] : '—', 'Email');
+  $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
